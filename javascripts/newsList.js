a87dafb2abe66aeefd12e178cf0bee81089572e2
g218suna <su.ikumi0408@gmial.com>
2022-05-30 18:36:29 +0900

    first

---
+++ javascripts/newsList.js
@@ -0,0 +1,14 @@
+class News {
+    constructor(date, genre, caption, link) {
+        this.date = date;
+        this.genre = genre;
+        this.caption = caption;
+        this.link = link;
+    }
+
+    createList() {
+
+    }
+}
+
+const newsList = new News();
